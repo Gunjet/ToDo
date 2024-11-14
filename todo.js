ae5a6c58df4083fad9d606bf08d4f52fdc9dcaf4
Gunjlkham08 <gunjegunje54@gmail.com>
2024-11-14 18:13:05 +0800

    added some changes

--- todo.js
+++ todo.js
@@ -4,110 +4,65 @@ let editIdx = 0;
 function renderOneList() {}
 
 function render() {
-  document.querySelector("#todo-tasks").innerHTML="";
-  document.querySelector("#in-progress-tasks").innerHTML="";
-  document.querySelector("#done-tasks").innerHTML="";
-  document.querySelector("#blocked-tasks").innerHTML="";
-  // console.log(taskList);
+  document.querySelector("#todo-tasks").innerHTML = "";
+  document.querySelector("#in-progress-tasks").innerHTML = "";
+  document.querySelector("#done-tasks").innerHTML = "";
+  document.querySelector("#blocked-tasks").innerHTML = "";
 
   for (let i = 0; i < todos.length; i++) {
     const containerName = todos[i].status;
-    // const todoList = document.getElementById(containerName);
     const containerNewName = "#" + containerName + "-tasks";
     const taskList = document.querySelector(containerNewName);
     const item = todos[i];
-
-    // create task item
-    // const x = document.createElement("input");
-    // x.setAttribute("type", "radio");
-    // x.classList.add("input");
-
-    // const element = document.createElement("div");
-    // element.classList.add("todo-item");
-
-    // create task name
-    // const titleEl = document.createElement("p");
-    // titleEl.innerText = item.name;
-
-    // create edit button
-    // const btnEl = document.createElement("button");
-    // btnEl.innerText = "Edit";
-    // btnEl.onclick = function() {
-    //   const modal = document.querySelector("#modal");
-    //   modal.style.display = "block";
-    //   const submitBtn = document.querySelector(".submit");
-    //   submitBtn.style.display = "none"
-    //   const editBtn = document.querySelector("#edit-button");
-    //   editBtn.style.display = "block";
-    //   editIdx = i;
-    // }
-
-    // const deleteBtn = document.createElement("button");
-    // deleteBtn.innerText = "Delete";
-    // deleteBtn.onclick = function () { 
-    // };
-
-// const taskHTML = `
-// <div class="todo-item">
-//   <input type="radio" class="input">
-//   <p>${item.name}</p>
-//   <button class="edit-btn">Edit</button>
-//   <button class="delete-btn">Delete</button>
-// </div>
-// `;
-const taskHTML=`
+    const taskHTML = `
 <div class="todo-item">
-<div class="todo-item2">
-  <input type="radio" class="input">
-  <p class="taskname">${item.name}</p>
-  <button class="edit-btn">
-  <img src="edit (1).png" class="icon">
-  </button>
-  <button class="delete-btn">
-    <img src="delete.png" class="icon">
-  </button>
-  </div>
-</div>
+      <div class="todo-item2">
+      <div style="display: flex; justify-content: start;
+       align-items: center; gap:10px; ">
+        <div class="task-circle"></div>
+        <p class="taskname">${item.name}</p>
+      </div>
+      
+        <div><button class="edit-btn">
+          <img src="edit (1).png" class="icon">
+        </button>
+        <button class="delete-btn">
+          <img src="delete.png" class="icon">
+        </button></div>
+        
+        </div>
+      </div>
 `;
 
-taskList.innerHTML += taskHTML;
+    taskList.innerHTML += taskHTML;
 
-const taskElement = taskList.querySelector(".todo-item:last-child");
+    console.log(todos[i])
 
-const editBtn = taskElement.querySelector(".edit-btn");
-editBtn.onclick = function () {
-const modal = document.querySelector("#modal");
-modal.style.display = "block";
-const submitBtn = document.querySelector(".submit");
-submitBtn.style.display = "none";
-const editBtn = document.querySelector("#edit-button");
-editBtn.style.display = "block";
-editIdx = i;
-};
+    const taskElement = taskList.querySelector(".todo-item:last-child");
 
-const deleteBtn = taskElement.querySelector(".delete-btn");
-deleteBtn.onclick = function () {
-deleteTask(i);  
-};
+    const editBtn = taskElement.querySelector(".edit-btn");
+    editBtn.onclick = function () {
+      const modal = document.querySelector("#modal");
+      modal.style.display = "block";
+      const submitBtn = document.querySelector(".submit");
+      submitBtn.style.display = "none";
+      const editBtn = document.querySelector("#edit-button");
+      editBtn.style.display = "block";
+      editIdx = i;
+    };
 
-    element.appendChild(x)
-    element.appendChild(titleEl);
-    element.appendChild(btnEl);
-    element.appendChild(deleteBtn);
-
-    taskList.appendChild(element);
 
     console.log(todos);
   }
 }
 
-
-
-
+function deleteTask(index) {
+  todos.splice(index, 1);
+  render();
+}
 function addTodo() {
   const modal = document.querySelector("#modal");
   modal.style.display = "block";
-
 }
 function saveTodo() {
   const inputValue = document.getElementById("task-name").value;
